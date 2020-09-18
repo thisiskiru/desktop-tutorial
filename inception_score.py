bb2b28f6fdc6a66f18e530d1f3b5953a138898ed
Kirubel Abebe <42034463+thisiskiru@users.noreply.github.com>
2020-09-18 12:03:32 +0300

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -1,78 +1,64 @@
-'''
-From https://github.com/tsc2017/Inception-Score
-Code derived from https://github.com/openai/improved-gan/blob/master/inception_score/model.py and https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/gan/python/eval/python/classifier_metrics_impl.py
-
-Usage:
-    Call get_inception_score(images, splits=10)
-Args:
-    images: A numpy array with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. A dtype of np.uint8 is recommended to save CPU memory.
-    splits: The number of splits of the images, default is 10.
-Returns:
-    Mean and standard deviation of the Inception Score across the splits.
-'''
-
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-import tensorflow_gan as tfgan
-import os
-import functools
+#calculate inception score with Keras
+from math import floor
+from numpy import ones, expand_dims, log, mean, std, exp
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
 import numpy as np
-import time
-from tensorflow.python.ops import array_ops
-# pip install tensorflow-gan
-import tensorflow_gan as tfgan
-session=tf.compat.v1.InteractiveSession()
-# A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
-BATCH_SIZE = 64
-INCEPTION_TFHUB = 'https://tfhub.dev/tensorflow/tfgan/eval/inception/1'
-INCEPTION_OUTPUT = 'logits'
+#Images need to have shape 299x299x3, pixels in [0,255]
+from numpy.random import shuffle
+import tensorflow as tf
+#from inception_score import get_inception_score
+import os 
+import cv2
+import numpy
 
-# Run images through Inception.
-inception_images = tf.compat.v1.placeholder(tf.float32, [None, 3, None, None], name = 'inception_images')
-def inception_logits(images = inception_images, num_splits = 1):
-    images = tf.transpose(images, [0, 2, 3, 1])
-    size = 299
-    images = tf.compat.v1.image.resize_bilinear(images, [size, size])
-    generated_images_list = array_ops.split(images, num_or_size_splits = num_splits)
-    logits = tf.map_fn(
-        fn = tfgan.eval.classifier_fn_from_tfhub(INCEPTION_TFHUB, INCEPTION_OUTPUT, True),
-        elems = array_ops.stack(generated_images_list),
-        parallel_iterations = 8,
-        back_prop = False,
-        swap_memory = True,
-        name = 'RunClassifier')
-    logits = array_ops.concat(array_ops.unstack(logits), 0)
-    return logits
+path = "D:/flower_result/Sun_to_Day/CC_/A2B/"
+folder = os.listdir(path)
+batch  = len(folder)
+shuffle(folder)
+folder = folder[:500]
+def calculate_inception_score(images, n_split=10, eps=1E-16):
+    #load inception model
+    model = InceptionV3()
+    #convert from uint8 to float32
+    #processed = images.astype('float32')
+    processed = np.array(images, dtype=np.float32)
+    # preprocess rae images from inception v3 model
+    processed = preprocess_input(processed)
+    #predict class probability from Images
+    yhat = model.predict(processed)
+    #enumerate splits of images/predictions
+    scores = list()
+    n_part = floor(len(images) / n_split)
+    for i in range(n_split):
+        #retrieve p(y|x)
+        ix_start, ix_end = i * n_part, i * n_part + n_part
+        p_yx = yhat[ix_start:ix_end]
+        #calculate p(Y)
+        p_y = expand_dims(p_yx.mean(axis=0),0)
+        #calculate KL divergence using log probabilities
+        kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))
+        # sum over classes
+        sum_kl_d = kl_d.sum(axis=1)
+        # average over Images
+        avg_kl_d = mean(sum_kl_d)
+        #undo the log
+        is_score = exp(avg_kl_d)
+        # store
+        scores.append(is_score)
+    #average across scores
+    is_avg, is_std = mean(scores), std(scores)
+    return is_avg, is_std
 
-logits=inception_logits()
 
-def get_inception_probs(inps):
-    session=tf.get_default_session()
-    n_batches = int(np.ceil(float(inps.shape[0]) / BATCH_SIZE))
-    preds = np.zeros([inps.shape[0], 1000], dtype = np.float32)
-    for i in range(n_batches):
-        inp = inps[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] / 255. * 2 - 1
-        preds[i * BATCH_SIZE : i * BATCH_SIZE + min(BATCH_SIZE, inp.shape[0])] = session.run(logits,{inception_images: inp})[:, :1000]
-    preds = np.exp(preds) / np.sum(np.exp(preds), 1, keepdims=True)
-    return preds
 
-def preds2score(preds, splits=10):
-    scores = []
-    for i in range(splits):
-        part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
-        kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
-        kl = np.mean(np.sum(kl, 1))
-        scores.append(np.exp(kl))
-    return np.mean(scores), np.std(scores)
+def batch_generator():
+    images = []
+    for i in range(len(folder)):
+        im = cv2.imread(path + folder[i])
+        im = cv2.resize(im, (299, 299)) 
+        images.append(im)
+    return images
+images = batch_generator()
 
-def get_inception_score(images, splits=10):
-    assert(type(images) == np.ndarray)
-    assert(len(images.shape) == 4)
-    assert(images.shape[1] == 3)
-    assert(np.min(images[0]) >= 0 and np.max(images[0]) > 10), 'Image values should be in the range [0, 255]'
-    print('Calculating Inception Score with %i images in %i splits' % (images.shape[0], splits))
-    start_time=time.time()
-    preds = get_inception_probs(images)
-    mean, std = preds2score(preds, splits)
-    print('Inception Score calculation time: %f s' % (time.time() - start_time))
-    return mean, std  # Reference values: 11.38 for 50000 CIFAR-10 training set images, or mean=11.31, std=0.10 if in 10 splits.
+is_Av , is_Std = calculate_inception_score(images)
+print("Avarage : ",is_Av , 'is_Std : ',is_Std)
